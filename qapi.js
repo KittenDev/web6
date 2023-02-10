b409697469c9df218dc23e591c53b9773a0fbe33
Dafa Alfarizki Pratama <78303775+KittenDev@users.noreply.github.com>
2023-02-10 09:28:08 +0700

    New Project Web UwU. Panggil saya kaptop

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63e5ab40f63de400267473a1",
+				"63e5ab40f63de400267473a4"
+			]
+		},
+		"63e5ab40f63de400267473a1": {
+			"id": "63e5ab40f63de400267473a1",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63e5ab40f63de400267473a4": {
+			"id": "63e5ab40f63de400267473a4",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
